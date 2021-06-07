1e3cff82a56312649a80fc84314b2235e736cfcc
Alec Johnson <alec@jumpstart.me>
2021-06-07 13:38:20 -0700

    auto approve merge PRs

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const core = require("@actions/core"),
-  github = require("@actions/github");
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-const token = core.getInput("github-token", { required: true }),
-  developBranch = core.getInput("develop"),
-  qaBranch = core.getInput("qa"),
-  mainBranch = core.getInput("main"),
-  client = new github.getOctokit(token);
+const token = core.getInput("github-token", { required: true });
+const developBranch = core.getInput("develop");
+const qaBranch = core.getInput("qa");
+const mainBranch = core.getInput("main");
+const client = new github.getOctokit(token);
 
 const approveSrcTarget = {
   [developBranch]: qaBranch,
@@ -14,12 +14,14 @@ const approveSrcTarget = {
 
 async function run() {
   try {
-    const pr =  github.context.payload.pull_request,
-      action = github.context.payload.action,
-      prSrc = pr.head.ref,
-      prTarget = pr.base.ref;
-      owner = github.context.repo.owner,
-      repo = github.context.repo.repo;
+    const pr =  github.context.payload.pull_request;
+    const action = github.context.payload.action;
+    const prSrc = pr.head.ref;
+    const prTarget = pr.base.ref;
+    const owner = github.context.repo.owner;
+    const repo = github.context.repo.repo;
+    const title = pr.title;
+    const pull_number = github.context.payload.pull_request.number;
 
     core.info(`Pull Request src: "${prSrc}", target: "${prTarget}"`);
 
@@ -27,7 +29,15 @@ async function run() {
       client.pulls.createReview({
         owner,
         repo,
-        pull_number: github.context.payload.pull_request.number,
+        pull_number: pull_number,
+        body: 'Looks good to me :shipit:',
+        event: 'APPROVE',
+      })
+    } else if (title.startsWith("Deploy: release v" && prTarget == developBranch)) {
+      client.pulls.createReview({
+        owner,
+        repo,
+        pull_number: pull_number,
         body: 'Looks good to me :shipit:',
         event: 'APPROVE',
       })
@@ -35,7 +45,7 @@ async function run() {
       client.pulls.createReview({
         owner,
         repo,
-        pull_number: github.context.payload.pull_request.number,
+        pull_number: pull_number,
         body: `I can't automatically approve changes from "${prSrc}" to "${prTarget}. A human will have to review this. :robot:`,
         event: 'COMMENT',
       })
